369b2240c255566a519c6877d9ac79f7ca675dec
raisyachmad <raisyaramadhani266@gmail.com>
2024-05-11 20:33:56 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$conn=mysqli_connect('localhost','root','','db_x');
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+    }else{
+        //echo "koneksi sukses", "<br>";
+    }
+?>
